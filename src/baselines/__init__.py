de77df355f9325c963d6d2bbe83403351e63f650
Sudip Kumar Paudel <sudipkumarpaudel@gmail.com>
2026-05-26 14:44:38 +0200

    Task 2: export voigt_to_tau_3x3 from baselines

--- src/baselines/__init__.py
+++ src/baselines/__init__.py
@@ -1,8 +1,8 @@
 from .smagorinsky import DynamicSmagorinsky
 from .wale import WALE
-from .beck_mlp import BeckMLP, EnergyConservingMLP, train_mlp, run_inference
+from .beck_mlp import BeckMLP, EnergyConservingMLP, train_mlp, run_inference, voigt_to_tau_3x3
 
 __all__ = [
     'DynamicSmagorinsky', 'WALE',
-    'BeckMLP', 'EnergyConservingMLP', 'train_mlp', 'run_inference',
+    'BeckMLP', 'EnergyConservingMLP', 'train_mlp', 'run_inference', 'voigt_to_tau_3x3',
 ]
